4ed918f8d5ebfa61830424f8a8fd6e017e52ff70
Akhilesh Samavedula <akhil.svr@hotmail.com>
2013-06-02 04:26:50 +0800

    Commiting DecompilerCheck files to Git to test it.

---
+++ DecompilerCheck/DecompilerCheck/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomLibrary;
+
+namespace DecompilerCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomStringSeq = "abcdefghijklmnopqrstuvwxyz".GenerateRandStringSeq(10).Take(10);
+            foreach(var str in randomStringSeq)
+                Console.WriteLine(str);
+
+            string readLine = Console.ReadLine();
+
+            Console.WriteLine("Check");
+        }
+    }
+}
---
+++ DecompilerCheck/RandomLibrary/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomLibrary
+{
+    public static class StringExtensions
+    {
+        public static IEnumerable<string> GenerateRandStringSeq(this string charColl, int strLen)
+        {
+            var rand = new Random();
+
+            int maxCharCount = charColl.Length;
+            var newStringCharColl = new char[strLen];
+
+            while (true)
+            {
+                for (int i = 0; i < strLen; i++)
+                {
+                    char nextChar = charColl[rand.Next(maxCharCount)];
+                    newStringCharColl[i] = nextChar;
+                }
+
+                yield return new string(newStringCharColl);
+            }
+
+            yield break;
+        }
+    }
+}
