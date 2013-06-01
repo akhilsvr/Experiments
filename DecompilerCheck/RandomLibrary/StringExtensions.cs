fc946025f2f9625f3800eee0c40dc635a9f268ce
Akhilesh Samavedula <akhil.svr@hotmail.com>
2013-06-02 05:09:21 +0800

    Testing Git 3

--- DecompilerCheck/RandomLibrary/StringExtensions.cs
+++ DecompilerCheck/RandomLibrary/StringExtensions.cs
@@ -6,18 +6,6 @@ using System.Threading.Tasks;
 
 namespace RandomLibrary
 {
-    public class TestClass
-    {
-        private int a;
-        private int b;
-
-        public TestClass()
-        {
-            a = 0;
-            b = 0;
-        }
-    }
-
     public static class StringExtensions
     {
         public static IEnumerable<string> GenerateRandStringSeq(this string charColl, int strLen)
---
+++ DecompilerCheck/RandomLibrary/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomLibrary
+{
+    public class TestClass
+    {
+        public string a;
+        public string b;
+
+        public TestClass(string aS, string bS)
+        {
+            a = aS;
+            b = bS;
+        }
+    }
+}
